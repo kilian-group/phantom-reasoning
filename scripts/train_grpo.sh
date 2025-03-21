8e4430a1e55d9b582ffd6ae88ae4e394b3b10e59
Anmol Kabra <ak2426@cornell.edu>
2025-03-20 21:42:12 -0400

    Add aida sbatch specs, --log_completions, --bf16

--- scripts/train_grpo.sh
+++ scripts/train_grpo.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=grpo
+#SBATCH --output=logs/grpo-%j.out
+#SBATCH --error=logs/grpo-%j.err
+#SBATCH -p full
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=100GB
+#SBATCH --time=24:00:00
 
 python -m phantom_reasoner.grpo \
     --dataset_name "kilian-group/phantom-wiki-v1" \
+    --split_name "depth_20_size_50_seed_1" \
     --model_name_or_path "Qwen/Qwen2.5-0.5B-Instruct" \
-    --num_train_epochs 3 \
+    --num_train_epochs 10 \
     --log_level "info" \
     --logging_strategy "steps" \
     --logging_first_step \
-    --logging_steps 100 \
+    --logging_steps 10 \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 100 \
     --save_total_limit 5 \
+    --log_completions \
+    --bf16 \
     $@
     # --use_vllm \
 
