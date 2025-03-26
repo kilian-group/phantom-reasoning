6667f13caf41f4fcbe76c446bfab33462e9b7053
Anmol Kabra <ak2426@cornell.edu>
2025-03-26 14:57:17 -0400

    Use vllm for generation

--- scripts/train_grpo_3b.sh
+++ scripts/train_grpo_3b.sh
@@ -5,7 +5,7 @@
 #SBATCH -p full
 #SBATCH -N 1
 #SBATCH -n 8
-#SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:a100:4
 #SBATCH --mem=100GB
 #SBATCH --time=24:00:00
 
@@ -13,16 +13,20 @@
 NUM_GPUS=$(nvidia-smi --query-gpu=count --format=csv,noheader,nounits | head -n 1)
 
 # --use_vllm reserves 1 GPU for generation, so then set NUM_PROCESSES=NUM_GPUS-1
-NUM_PROCESSES=$NUM_GPUS
+NUM_PROCESSES=$((NUM_GPUS - 1))
 
 # TODO: Figure out why vllm server keeps crashing.
 # Till then we train without vllm generations.
 # Start a vllm server in a separate terminal
 # CUDA_VISIBLE_DEVICES=3 trl vllm-serve --model "Qwen/Qwen2.5-3B-Instruct" &
 
+# Get CUDA visible devices as 0,...,NUM_GPUS-2 (0 indexing, and last one is reserved for vllm)
+CUDA_DEVICES=$(seq -s, 0 $((NUM_GPUS - 2)))
+
 # 3B model on 4 A100s (320GB GPU memory) works with zero1 with lora
 # Start the training script on the GPUs
-ACCELERATE_LOG_LEVEL=info accelerate launch \
+export WANDB_PROJECT="grpo"
+CUDA_VISIBLE_DEVICES=$CUDA_DEVICES ACCELERATE_LOG_LEVEL=info accelerate launch \
     --num_processes=$NUM_PROCESSES \
     --config_file recipes/accelerate_configs/zero1.yaml \
 	src/phantom_reasoner/grpo.py \
