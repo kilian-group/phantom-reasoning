cf43adfc5c87b4dfe8d7ad67453a2154e90129ae
Anmol Kabra <ak2426@cornell.edu>
2025-08-19 12:12:54 -0400

    Update eval scripts with new model names (Anmol iterates this on regularly)

--- scripts/eval/other_eval_grpo.sh
+++ scripts/eval/other_eval_grpo.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 # NOTE: hacked together
 # Script to run the Qwen3 family of models on the Wiki datasets (HP, 2Wiki, MSQ)
-# Usage: ./eval_grpo_on_other_datasets.sh <output_dir> <split>
+# Usage: ./eval/other_eval_grpo.sh <output_dir> <dataset> <split>
 # Split can be minidev (500 examples) or dev (~11K examples)
 
 OUTPUT_DIR=$1
-SPLIT=$2
+dataset=$2
+SPLIT=$3
 
 DATASET_LIST=(
     "hp500"
     "2wiki500"
     "msq500"
 )
+
+# If dataset not in DATASET_LIST, complain
+if [[ ! " ${DATASET_LIST[@]} " =~ " ${dataset} " ]]; then
+    echo "Dataset $dataset not in DATASET_LIST"
+    exit 1
+fi
+
 MODEL_NAMES=(
     "Qwen/Qwen3-0.6B"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0804__curr=random__prompt=cot/"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0804__curr=difficulty_asc__prompt=cot/"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0813__curr=random__prompt=cot"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0814__curr=difficulty_asc__prompt=cot"
     "Qwen/Qwen3-1.7B"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-1.7B/grpo/ak2426/0804__curr=random__prompt=cot/"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-1.7B/grpo/ak2426/0804__curr=difficulty_asc__prompt=cot/"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-1.7B/grpo/ak2426/0814__curr=random__prompt=cot"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-1.7B/grpo/ak2426/0814__curr=difficulty_asc__prompt=cot"
+    "Qwen/Qwen2.5-1.5B-Instruct"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen2.5-1.5B-Instruct/grpo/ak2426/0814__curr=random__prompt=cot"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen2.5-1.5B-Instruct/grpo/ak2426/0815__curr=difficulty_asc__prompt=cot"
+    "google/gemma-3-1b-it"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/google/gemma-3-1b-it/grpo/ak2426/0815__curr=random__prompt=cot"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/google/gemma-3-1b-it/grpo/ak2426/0815__curr=difficulty_asc__prompt=cot"
+    "meta-llama/Llama-3.2-3B-Instruct"
+    "runs__answertag/data/wiki-v1-easy-depth_20_size_25/meta-llama/Llama-3.2-3B-Instruct/grpo/ak2426/0816__curr=difficulty_asc__prompt=cot"
 )
 
-for dataset in ${DATASET_LIST[@]}; do
-    for model_name in ${MODEL_NAMES[@]}; do
-        # NOTE: thinking mode is on by default
-        CUDA_VISIBLE_DEVICES=0 python examples/wiki/pred.py \
-          --data_dir data/ \
-          --dataset $dataset \
-          --split $SPLIT \
-          --method cot \
-          --server vllm \
-          --model_name $model_name \
-          --inf_temperature 0.6 \
-          --inf_top_p 0.95 \
-          --inf_top_k 20 \
-          -od $OUTPUT_DIR \
-          --inf_vllm_tensor_parallel_size 1
-    done
+for model_name in ${MODEL_NAMES[@]}; do
+    # NOTE: thinking mode is on by default
+    CUDA_VISIBLE_DEVICES=0 python examples/wiki/pred.py \
+        --data_dir data/ \
+        --dataset $dataset \
+        --split $SPLIT \
+        --method cot \
+        --server vllm \
+        --model_name $model_name \
+        --inf_temperature 0.6 \
+        --inf_top_p 0.95 \
+        --inf_top_k 20 \
+        -od $OUTPUT_DIR \
+        --inf_vllm_tensor_parallel_size 1
 done
--- scripts/eval/pw_eval_grpo.sh
+++ scripts/eval/pw_eval_grpo.sh
@@ -3,19 +3,14 @@
 # Script to run the Qwen3 family of models on the PhantomWiki datasets
 # Usage: ./scripts/eval/pw_eval_grpo.sh <output_dir>
 
-OUTPUT_DIR="out-0804"
+OUTPUT_DIR=$1
 
 MODEL_NAMES=(
-    "Qwen/Qwen3-0.6B"
-    "Qwen/Qwen3-1.7B"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0804__curr=random__prompt=cot/"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0804__curr=difficulty_asc__prompt=cot/"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-1.7B/grpo/ak2426/0804__curr=random__prompt=cot/"
-    "runs__bs=1/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-1.7B/grpo/ak2426/0804__curr=difficulty_asc__prompt=cot/"
+    "runs__test_answertag/data/wiki-v1-easy-depth_20_size_25/Qwen/Qwen3-0.6B/grpo/ak2426/0818__curr=random__prompt=cot"
 )
 
-# for MODEL_NAME in ${MODEL_NAMES[@]}
-# do
+for MODEL_NAME in ${MODEL_NAMES[@]}
+do
 #   # Create a temporary script file for this model
 #   SCRIPT_FILE="scripts/run_phantom_eval_$(basename "${MODEL_NAME//\//_}").sh"
 
@@ -38,7 +33,6 @@ MODEL_NAMES=(
 #   --split_list depth_20_size_25_seed_1 depth_20_size_25_seed_2 depth_20_size_25_seed_3 \
 #   --from_local \
 #   --exclude_aggregation_questions \
-#   --inf_is_deepseek_r1_model \
 #   --inf_vllm_tensor_parallel_size 1 \
 #   -od "${OUTPUT_DIR}"
 # EOF
@@ -62,19 +56,18 @@ MODEL_NAMES=(
 # MODEL_NAME=$1
 # OUTPUT_DIR=$2
 
-# python -m phantom_eval \
-#   --method cot \
-#   --server vllm \
-#   --inf_vllm_offline \
-#   --model_name "${MODEL_NAME}" \
-#   --dataset data/wiki-v1-easy-depth_20_size_25 \
-#   --split_list depth_20_size_25_seed_1 depth_20_size_25_seed_2 depth_20_size_25_seed_3 \
-#   --from_local \
-#   --exclude_aggregation_questions \
-#   --inf_is_deepseek_r1_model \
-#   --inf_temperature 1 \
-#   --inf_vllm_tensor_parallel_size 1 \
-#   -od "${OUTPUT_DIR}__temp=1.0"
+CUDA_VISIBLE_DEVICES=0 python -m phantom_eval \
+  --method cot \
+  --server vllm \
+  --inf_vllm_offline \
+  --model_name "${MODEL_NAME}" \
+  --dataset data/wiki-v1-easy-depth_20_size_25 \
+  --split_list depth_20_size_25_seed_1 depth_20_size_25_seed_2 depth_20_size_25_seed_3 \
+  --from_local \
+  --exclude_aggregation_questions \
+  --inf_temperature 1 \
+  --inf_vllm_tensor_parallel_size 1 \
+  -od "${OUTPUT_DIR}__temp=1.0"
 # EOF
 
 #   # Make the script executable
@@ -82,21 +75,24 @@ MODEL_NAMES=(
 
 #   # Submit the job with the model name as an environment variable
 #   sbatch -p full -n 8 -t 2:00:00 --gres=gpu:a100:1 --mem=50GB "$SCRIPT_FILE" "$MODEL_NAME" "$OUTPUT_DIR"
-# done
+  echo "done"
+done
 
 echo "${MODEL_NAMES[*]}"
 
-python ~/work/phantom-wiki/eval/format_leaderboard.py \
-  -od $OUTPUT_DIR \
-  --model_list "${MODEL_NAMES[*]}" \
-  --size_list 25 \
-  --method_list cot \
-  --dataset data/wiki-v1-easy-depth_20_size_25 \
-  --from_local
+rm -r cachedir/
+
+# python ~/work/phantom-wiki/eval/format_leaderboard.py \
+#   -od $OUTPUT_DIR \
+#   --model_list ${MODEL_NAMES[*]} \
+#   --size_list 25 \
+#   --method_list cot \
+#   --dataset data/wiki-v1-easy-depth_20_size_25 \
+#   --from_local
 
 python ~/work/phantom-wiki/eval/format_leaderboard.py \
   -od "${OUTPUT_DIR}__temp=1.0" \
-  --model_list "${MODEL_NAMES[*]}" \
+  --model_list ${MODEL_NAMES[*]} \
   --size_list 25 \
   --method_list cot \
   --dataset data/wiki-v1-easy-depth_20_size_25 \
