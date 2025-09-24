8a3b5c77599511455d70e30065a83742f8bf4c28
anmolkabra <ak2426@cornell.edu>
2025-09-23 21:32:22 -0400

    Play with ticklabels and spacing

--- examples/wiki/plot_all_wiki_scaling_final_ckpts.py
+++ examples/wiki/plot_all_wiki_scaling_final_ckpts.py
@@ -20,7 +20,9 @@ import os
 
 import matplotlib.lines as lines
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import yaml
+from phantom_eval.evaluate_utils import mean, std
 from utils.data_utils import get_parser
 from utils.evaluate_utils import get_preds
 
@@ -28,6 +30,7 @@ from phantom_reasoner.utils import plotting_utils
 
 parser = get_parser()
 parser.add_argument("--final_ckpts_yaml_path", type=str, required=True)
+parser.add_argument("--base_model_names_to_plot", nargs="+", default=["Qwen/Qwen3-0.6B", "Qwen/Qwen3-1.7B"])
 args = parser.parse_args()
 
 eval_datasets = ["hp500", "2wiki500", "msq500"]
@@ -42,14 +45,14 @@ eval_dataset2plot_metric = {
     "msq500": "f1",
 }
 eval_dataset2ylims = {
-    "hp500": (0.25, 0.75),
-    "2wiki500": (0.25, 0.75),
+    "hp500": (0.3, 0.8),
+    "2wiki500": (0.3, 0.8),
     "msq500": (0.0, 0.5),
 }
 eval_dataset2yticks = {
-    "hp500": [0.25, 0.5, 0.75],
-    "2wiki500": [0.25, 0.5, 0.75],
-    "msq500": [0.0, 0.25, 0.5],
+    "hp500": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+    "2wiki500": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+    "msq500": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],
 }
 
 # Load the yaml file
@@ -60,7 +63,8 @@ with open(args.final_ckpts_yaml_path) as f:
 training_dataset_names: list[str] = [train_dataset["dataset_name"] for train_dataset in synthetic_train_ckpts]
 base_model_names: set[str] = set()
 
-fig, axes = plt.subplots(1, len(eval_datasets), figsize=(len(eval_datasets) * 4, 4))
+fig_width = len(eval_datasets) * 4
+fig, axes = plt.subplots(1, len(eval_datasets), figsize=(fig_width, 4))
 
 # Create a subfigure for each dataset
 for i, eval_dataset in enumerate(eval_datasets):
@@ -76,8 +80,7 @@ for i, eval_dataset in enumerate(eval_datasets):
 
         for ckpt in train_ckpts_dict["ckpts"]:
             base_model_name = ckpt["model"]
-            if base_model_name == "Qwen/Qwen2.5-1.5B-Instruct":
-                # Do not plot Qwen2.5-1.5B-Instruct because it starts at 0, so the plot will be weird
+            if base_model_name not in args.base_model_names_to_plot:
                 continue
             base_model_names.add(base_model_name)
             ckpt_path = ckpt["paths"][0]  # NOTE: Take the first model path only
@@ -98,56 +101,42 @@ for i, eval_dataset in enumerate(eval_datasets):
 
             df_preds["completion_tokens"] = df_preds["usage"].apply(lambda x: x["completion_tokens"])
 
-            # Define aggregation functions
-            agg_dict = {
-                **{metric: "mean" for metric in metrics},
-                "completion_tokens": [
-                    "mean",
-                    lambda x: x.quantile(0.5),
-                ],
-            }
-
-            acc = df_preds.groupby(["_model", "_split", "_seed", "checkpoint_number"]).agg(agg_dict)
-
-            # Flatten column names
-            acc.columns = metrics + [
-                "completion_tokens_mean",
-                "completion_tokens_median",
-            ]
-
-            # print(tabulate(acc, headers="keys", tablefmt="github"))
-            # save to csv
-            # scores_dir = os.path.join(output_dir, "scores")
-            # os.makedirs(scores_dir, exist_ok=True)
-            # scores_path = os.path.join(scores_dir, f"{eval_dataset}_{split}_{method}.csv")
-            # acc.to_csv(scores_path)
-            # print(f"Saved scores to {scores_path}")
+            acc = df_preds.groupby(["_model", "_split", "_seed", "checkpoint_number"])[metrics].agg(
+                [mean, std]
+            )
+            acc = acc.reset_index().sort_values(by="checkpoint_number")
+            ckpt_numbers = acc["checkpoint_number"].values
 
             # Plot a line chart of metric vs checkpoint number
-            # acc is a multi-index dataframe, with index as (_model, _split, _seed, checkpoint_number)
             metric = eval_dataset2plot_metric[eval_dataset]
-            metric_data = acc[metric].sort_index(level="checkpoint_number")
+            metric_data = acc[metric]
             marker = plotting_utils.MODEL_NAME2MARKER[base_model_name]  # based on model name
             color = plotting_utils.COLORS2HEX[
                 plotting_utils.TRAIN_DATASET_ALIAS2COLOR[train_dataset_name]
             ]  # based on pw or gsminf
             ax.plot(
-                metric_data.index.get_level_values("checkpoint_number"),
-                metric_data,
+                ckpt_numbers,
+                metric_data["mean"],
                 marker=marker,
                 color=color,
                 linestyle="solid",
                 linewidth=plotting_utils.LINE_WIDTH,
             )
-            max_ckpt_number = max(
-                max_ckpt_number, max(metric_data.index.get_level_values("checkpoint_number"))
+            ax.fill_between(
+                ckpt_numbers,
+                metric_data["mean"] - metric_data["std"],
+                metric_data["mean"] + metric_data["std"],
+                alpha=0.1,
+                color=color,
             )
+            max_ckpt_number = max(max_ckpt_number, max(ckpt_numbers))
 
     ax.set_xlabel("Training steps", fontsize=plotting_utils.LABEL_FONT_SIZE)
     if i == 0:
         ax.set_ylabel(metric.upper(), fontsize=plotting_utils.LABEL_FONT_SIZE)
     ax.set_title(eval_dataset2name[eval_dataset], fontsize=plotting_utils.LABEL_FONT_SIZE, fontweight="bold")
     ax.set_xlim(1, max_ckpt_number)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f"{int(x // 1000):d}" + "K"))
     ax.set_ylim(eval_dataset2ylims[eval_dataset])
     ax.set_yticks(eval_dataset2yticks[eval_dataset])
     # Add horizontal grid lines
@@ -155,16 +144,7 @@ for i, eval_dataset in enumerate(eval_datasets):
     ax.set_axisbelow(True)
 
 # Add a legend on the right side of the figure
-handles = [
-    lines.Line2D(
-        [],
-        [],
-        color="black",
-        marker="None",
-        label="Train dataset",
-        linestyle="None",
-    )
-]
+handles = []
 for training_dataset_name in training_dataset_names:
     # Add the training dataset name as a legend handle with no line or marker
     handles.append(
@@ -196,24 +176,23 @@ for training_dataset_name in training_dataset_names:
 legend = fig.legend(
     handles=handles,
     fontsize=plotting_utils.LEGEND_FONT_SIZE,
-    loc="center left",
-    ncol=1,
+    loc="upper center",
+    ncol=len(training_dataset_names),
     frameon=True,
     fancybox=False,
     edgecolor="black",
-    bbox_to_anchor=(0.9, 0.5),
+    bbox_to_anchor=(0.49, -0.02),
     bbox_transform=fig.transFigure,
+    title="Train dataset",
+    title_fontsize=plotting_utils.LEGEND_FONT_SIZE,
 )
-# Make the train dataset name bold
-text_object = legend.get_texts()[0]
-text_object.set_fontweight("bold")
 
 # Make the training dataset names bold and their specific color
 for i, training_dataset_name in enumerate(training_dataset_names):
     # There are 3 rows per training dataset name in the legend:
     # 1. The training dataset name
     # ... rest are base models
-    text_object = legend.get_texts()[i * (len(base_model_names) + 1) + 1]
+    text_object = legend.get_texts()[i * (len(base_model_names) + 1)]
 
     # Modify its style
     text_object.set_color(
@@ -227,11 +206,11 @@ plt.subplots_adjust(
     top=0.9,  # where the top subplot edges are, increase to move them closer to top figure edge
     bottom=0.1,  # where the bottom subplot edges are, increase to move them closer to bottom figure edge
     hspace=0.2,  # horizontal space between subplots, increase to move them away
-    wspace=0.2,  # vertical space between subplots, increase to move them away
+    wspace=0.15,  # vertical space between subplots, increase to move them away
 )
 
-save_path = os.path.join("f1_v_training_steps.pdf")
-# os.makedirs(os.path.dirname(save_path), exist_ok=True)
+str_for_model_names = "__".join([m.replace("/", "--") for m in args.base_model_names_to_plot])
+save_path = os.path.join(f"f1_v_training_steps_{str_for_model_names}.pdf")
 plt.savefig(save_path, bbox_inches="tight", dpi=300)
 print(f"Saved scaling plot to {save_path}")
 
