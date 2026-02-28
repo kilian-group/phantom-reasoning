d96b3bb4d15cdc50bde873af56466aef4f1b39fa
anmolkabra <ak2426@cornell.edu>
2026-02-28 13:15:07 -0500

    Remove old src files

--- src/phantom_reasoner/utils/callbacks.py
+++ src/phantom_reasoner/utils/callbacks.py
@@ -10,65 +10,10 @@ from transformers.training_args import TrainingArguments
 
 from phantom_reasoner.configs import GRPOScriptArguments
 
-from .evaluation import run_benchmark_jobs
-from .hub import push_to_hub_revision
-
 logger = logging.getLogger(__name__)
 
 
-##################################
-# START Callbacks that take in ModelConfig used by SFT trainer
-##################################
-def is_slurm_available() -> bool:
-    # returns true if a slurm queueing system is available
-    try:
-        subprocess.run(["sinfo"], check=True, capture_output=True)
-        return True
-    except FileNotFoundError:
-        return False
-
-
-class DummyConfig:
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-
-class PushToHubRevisionCallback(TrainerCallback):
-    def __init__(self, model_config) -> None:
-        self.model_config = model_config
-
-    def on_save(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
-        if state.is_world_process_zero:
-            global_step = state.global_step
-
-            # WARNING: if you use dataclasses.replace(args, ...) the accelerator dist state will be broken,
-            # so I do this workaround
-            # Also if you instantiate a new SFTConfig, the accelerator dist state will be broken
-            dummy_config = DummyConfig(
-                hub_model_id=args.hub_model_id,
-                hub_model_revision=f"{args.hub_model_revision}-step-{global_step:09d}",
-                output_dir=f"{args.output_dir}/checkpoint-{global_step}",
-                system_prompt=args.system_prompt,
-            )
-
-            future = push_to_hub_revision(
-                dummy_config, extra_ignore_patterns=["*.pt"]
-            )  # don't push the optimizer states
-
-            if is_slurm_available():
-                dummy_config.benchmarks = args.benchmarks
-
-                def run_benchmark_callback(_):
-                    print(f"Checkpoint {global_step} pushed to hub.")
-                    run_benchmark_jobs(dummy_config, self.model_config)
-
-                future.add_done_callback(run_benchmark_callback)
-
-
-CALLBACKS = {
-    "push_to_hub_revision": PushToHubRevisionCallback,
-}
+CALLBACKS: dict[str, TrainerCallback] = {}
 
 
 def get_callbacks(train_config, model_config) -> list[TrainerCallback]:
